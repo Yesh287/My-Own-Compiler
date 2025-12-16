ab7c22eee4c9c8e91eeeff60d9a72ce94486af89
Yesh287 <yashagarwal287@gmail.com>
2025-12-16 16:30:54 +0000

    Made tokenization more clean and into the hpp header file parsing is still left

--- src/main.cpp
+++ src/main.cpp
@@ -5,63 +5,8 @@
 #include <optional>
 #include <vector>
 #include "./tokenization.hpp"
-enum class TokenType {
-    exit,
-    int_lit,
-    semi
-};
 
-struct Token {
-    TokenType type;
-    std::optional <std::string> value;
-};
-
-std::vector<Token> tokenize (const std::string &str) {
-    std::vector <Token> tokens;
-    std::string buf;
-    for(int i = 0; i < str.length(); i++) {
-        char c = str.at(i);
-        if (std::isalpha(c)) {
-            buf.push_back(c);
-            i++;
-            while(std::isalnum(str.at(i))) {
-                buf.push_back(str.at(i));
-                i++;
-            }
-            i--;
-            if(buf == "exit") {
-                tokens.push_back({.type = TokenType::exit});
-                buf.clear();
-                continue;
-            } else {
-                std::cerr << "NO PLEASE NO!" << std::endl;
-                exit(EXIT_FAILURE);
-            }
-        }
-        else if (std::isdigit(c)) {
-            buf.push_back(c);
-            i++;
-            while (std::isdigit(str.at(i))) {
-                buf.push_back(str.at(i));
-                i++;
-            }
-            i--;
-            tokens.push_back({.type = TokenType::int_lit, .value =  buf});
-        }
-        else if (c == ';') {
-            tokens.push_back({.type = TokenType::semi});
-        }
-        else if (std::isspace(c)) {
-            continue;
-        } else {
-            std::cerr << "NO PLEASE NO!" << std::endl;
-            exit(EXIT_FAILURE);
-        }
-    }
-    return tokens;
-}
-
-std::string tokensToAsm (std::vector<Token>& tokens) {
+std::string tokens_to_asm (std::vector<Token>& tokens) {
     std::stringstream output;
     output << "global _start\n_start:\n";
     for(int i = 0; i < tokens.size(); i++) {
@@ -93,11 +38,11 @@ int main(int argc, char* argv[]) {
         contents_stream << input.rdbuf();
         contents = contents_stream.str();
     }
-
-    std::vector<Token> tokens = tokenize(contents);
+    Tokenizer tokenizer(contents);
+    std::vector<Token> tokens = tokenizer.tokenize();
     {
         std::fstream file("./out.asm", std::ios::out);
-        file << tokensToAsm(tokens);
+        file << tokens_to_asm(tokens);
     }
 
     system("nasm -felf64 ./out.asm");
